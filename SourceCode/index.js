eb92c47196c9bf964b7afabb959a84da201c4bb7
Dhia BEN HAMOUDA <benhamoudadhia@isimm.u-monastir.tn>
2024-11-09 15:08:50 +0000

    small update

--- SourceCode/index.js
+++ SourceCode/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-	res.send('<h1> ISIMM 24/25 : ING2 TD1</h1><h3> Let\'s see if BuildPush.yml action is setup correctly! Los geht\'s 3!</h3>')
+	res.send('<h1> ISIMM 24/25: ING2 - Semestre 1</h1><h3> Let\'s see if BuildPush.yml action is setup correctly! Los geht\'s 3!</h3>')
 });
 
 app.listen(8080, ()=>{
